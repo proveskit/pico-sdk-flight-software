0d486ce91596b5e36700fa058315238b6a838352
Michael Pham <61564344+Mikefly123@users.noreply.github.com>
2024-10-16 11:51:06 -0700

    Configured a new PicoSDK Project

--- Batt_Pico_sdk/build/pico-sdk/cmake_install.cmake
+++ Batt_Pico_sdk/build/pico-sdk/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Program Files/Raspberry Pi/Pico SDK v1.5.1/pico-sdk
+# Install script for directory: /Users/ncc-michael/.pico-sdk/sdk/2.0.0
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/pico_extras")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -34,15 +34,21 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/Raspberry Pi/Pico SDK v1.5.1/gcc-arm-none-eabi/bin/arm-none-eabi-objdump.exe")
+  set(CMAKE_OBJDUMP "/Users/ncc-michael/.pico-sdk/toolchain/13_2_Rel1/bin/arm-none-eabi-objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/nicol/OneDrive/Documents/GitHub/proves-tests/Batt_Pico_sdk/build/pico-sdk/tools/cmake_install.cmake")
-  include("C:/Users/nicol/OneDrive/Documents/GitHub/proves-tests/Batt_Pico_sdk/build/pico-sdk/src/cmake_install.cmake")
-  include("C:/Users/nicol/OneDrive/Documents/GitHub/proves-tests/Batt_Pico_sdk/build/pico_extras/src/cmake_install.cmake")
-  include("C:/Users/nicol/OneDrive/Documents/GitHub/proves-tests/Batt_Pico_sdk/build/pico-sdk/docs/cmake_install.cmake")
+  # Include the install script for the subdirectory.
+  include("/Users/ncc-michael/GitHut/pico-sdk-flight-software/Batt_Pico_sdk/build/pico-sdk/tools/cmake_install.cmake")
+endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/ncc-michael/GitHut/pico-sdk-flight-software/Batt_Pico_sdk/build/pico-sdk/src/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/ncc-michael/GitHut/pico-sdk-flight-software/Batt_Pico_sdk/build/pico-sdk/docs/cmake_install.cmake")
 endif()
 
